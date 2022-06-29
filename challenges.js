792af665ee4e5193b58274abad3050e22b394fa1
kennethachan <kennethandrechan@gmail.com>
2022-06-28 23:05:06 -0400

    challenge 12 complete

--- challenges.js
+++ challenges.js
@@ -319,7 +319,22 @@ hammingDistance('!!!!', '****'); //=> 4
 hammingDistance('abc', 'ab'); //=> NaN
 -----------------------------------------------------------------*/
 // Your solution for 12-hammingDistance here:
-function hammingDistance(str1, str2) {}
+const str1 = "!!!!"
+const str2 = "****"
+
+function hammingDistance(str1 = "", str2 = "") {
+  if (str1.length !== str2.length) {
+    return 0
+  }
+  let distance = 0
+  for (i = 0; i < str1.length; i += 1) {
+    if (str1[i] !== str2[i]) {
+      distance += 1
+    }
+  }
+  return distance
+}
+console.log(hammingDistance(str1, str2))
 /*-----------------------------------------------------------------
 Challenge: 13-mumble
 
--- testfile.js
+++ testfile.js
@@ -1,10 +1,16 @@
-function isPalindrome(string) {
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] !== string[string.length - 1 - i]) {
-      return false
+function hammingDistance(str1 = "", str2 = "") {
+  if (str1.length !== str2.length) {
+    return 0
+  }
+  let distance = 0
+  for (i = 0; i < str1.length; i += 1) {
+    if (str1[i] !== str2[i]) {
+      distance += 1
     }
   }
-  return true
+  return distance
 }
-const string = "bob"
-console.log(isPalindrome(string))
+console.log(hammingDistance(str1, str2))
+
+const str1 = "!!!!"
+const str2 = "****"
