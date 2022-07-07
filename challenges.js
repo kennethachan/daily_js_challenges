cb2c0075b2f7a5345e627589271981dfe13f8b18
kennethachan <kennethandrechan@gmail.com>
2022-07-06 21:23:54 -0400

    challenge 19 complete

--- challenges.js
+++ challenges.js
@@ -576,7 +576,17 @@ flatten( [1, [2, [3, [4]]], 1, 'a', ['b', 'c']] );
 //=> [1, 2, 3, 4, 1, 'a', 'b', 'c']
 -----------------------------------------------------------------*/
 // Your solution for 19-flatten here:
-function flatten(arr) {}
+function flatten(arr) {
+  let flatArr = []
+  arr.forEach(function (elem) {
+    if (Array.isArray(elem)) {
+      flatArr = flatArr.concat(flatten(elem))
+    } else {
+      flatArr.push(elem)
+    }
+  })
+  return flatArr
+}
 /*-----------------------------------------------------------------
 Challenge: 20-isPrime
 
